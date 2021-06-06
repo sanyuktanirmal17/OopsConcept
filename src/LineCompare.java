344bdd37fdac2c92f8bdf1f8802eb5a26cf29a51
sanyuktanirmal17 <sanyuktanirmal17@gmail.com>
2021-06-07 00:35:54 +0530

    Added: Compare Lines

--- src/LineCompare.java
+++ src/LineCompare.java
@@ -1,32 +1,46 @@
 
 public class LineCompare {
 
-		public static double dist(int a, int b, int a1, int b1) {
-			double length;
-			length = Math.sqrt(Math.pow((a1 - a), 2) + Math.pow((b1 - b), 2));
-			return length;
+	public static double dist(int a, int b, int a1, int b1) {
+		double length;
+		length = Math.sqrt(Math.pow((a1 - a), 2) + Math.pow((b1 - b), 2));
+		return length;
 
-		}
+	}
 
-		void checkLength() {
-			double line1 = dist(1, 2, 3, -4);
-			double line2 = dist(1, 2, 3, -4);
-			if (line1 == line2)
-				System.out.println("Lines are equal");
-			else
-				System.out.println("Lines are not equal");
-		}
+	void checkLength() {
+		double line1 = dist(1, 2, 3, -4);
+		double line2 = dist(1, 2, 3, -4);
+		if (line1 == line2)
+			System.out.println("Lines are equal");
+		else
+			System.out.println("Lines are not equal");
+	}
 
-		static class MainMethod {
-			public static void main(String args[]) {
-				{
-					System.out.println("Welcome to line comparision Computation ");
+	void lengthCompare() {
+		double line1 = dist(1, 2, 3, -4);
+		double line2 = dist(1, 2, 3, -4);
 
-					System.out.println(dist(1, 2, 3, -4));
-				}
-				LineCompare c1 = new LineCompare();
-				c1.checkLength();
+		if (line1 == line2)
+			System.out.println("Lines are equal");
+		else if (line1 > line2)
+			System.out.println("Lines are not equal, line1 > line2");
+		else
+			System.out.println("Lines are not equal line1 , line2 >");
+
+	}
+
+	static class MainMethod {
+		public static void main(String args[]) {
+			{
+				System.out.println("Welcome to line comparision Computation ");
+				System.out.println(dist(1, 2, 3, -4));
 			}
+			LineCompare c1 = new LineCompare();
+			LineCompare c2 = new LineCompare();
+
+			c1.checkLength();
+			c2.lengthCompare();
 		}
 	}
 }
