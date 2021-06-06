11c6af8e181a71840fd5502512588d2712bee272
sanyuktanirmal17 <sanyuktanirmal17@gmail.com>
2021-06-07 00:22:18 +0530

    Added: Welcome message

---
+++ Welcome.java
@@ -0,0 +1,2 @@
+#Welcome to  Line compare computaion 
+
---
+++ src/LineCompare.java
@@ -0,0 +1,19 @@
+
+public class LineCompare {
+
+	public static double dist(int a, int b, int a1, int b1) {
+		double length;
+		length = Math.sqrt(Math.pow((a1 - a), 2) + Math.pow((b1 - b), 2));
+		return length;
+	}
+
+	static class MainMethod {
+		public static void main(String args[]) {
+			{
+				System.out.println("Welcome to line comparision Computation ");
+
+			}
+
+		}
+	}
+}
