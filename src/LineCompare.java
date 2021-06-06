f1742fdfe451005d6031711019e145d7dc388d00
sanyuktanirmal17 <sanyuktanirmal17@gmail.com>
2021-06-07 00:29:50 +0530

    Added: Line Equality

--- src/LineCompare.java
+++ src/LineCompare.java
@@ -1,19 +1,32 @@
 
 public class LineCompare {
 
-	public static double dist(int a, int b, int a1, int b1) {
-		double length;
-		length = Math.sqrt(Math.pow((a1 - a), 2) + Math.pow((b1 - b), 2));
-		return length;
-	}
+		public static double dist(int a, int b, int a1, int b1) {
+			double length;
+			length = Math.sqrt(Math.pow((a1 - a), 2) + Math.pow((b1 - b), 2));
+			return length;
 
-	static class MainMethod {
-		public static void main(String args[]) {
-			{
-				System.out.println("Welcome to line comparision Computation ");
+		}
 
+		void checkLength() {
+			double line1 = dist(1, 2, 3, -4);
+			double line2 = dist(1, 2, 3, -4);
+			if (line1 == line2)
+				System.out.println("Lines are equal");
+			else
+				System.out.println("Lines are not equal");
+		}
+
+		static class MainMethod {
+			public static void main(String args[]) {
+				{
+					System.out.println("Welcome to line comparision Computation ");
+
+					System.out.println(dist(1, 2, 3, -4));
+				}
+				LineCompare c1 = new LineCompare();
+				c1.checkLength();
 			}
-
 		}
 	}
 }
